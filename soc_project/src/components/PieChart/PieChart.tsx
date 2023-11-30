b662b10f0769de911070f993345f60e07f259876
manarfareh <farehmanar89@gmail.com>
2023-11-30 17:44:12 +0100

    Pie chart

---
+++ soc_project/src/components/PieChart/PieChart.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { PieChartWrapper } from './PieChart.styled';
+
+interface PieChartProps {}
+
+const PieChart: FC<PieChartProps> = () => (
+ <PieChartWrapper>
+    PieChart Component
+ </PieChartWrapper>
+);
+
+export default PieChart;
